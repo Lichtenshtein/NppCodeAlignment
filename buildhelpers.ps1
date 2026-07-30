2661f04e789c2ee9590ef2d3499fab9bddada783
Lichtenshtein <altruumann@protonmail.com>
2026-07-30 21:14:14 +0300

    fix files packing

--- buildhelpers.ps1
+++ buildhelpers.ps1
@@ -24,7 +24,6 @@ function UpdateVsVersion($project, $version)
             "Id=`"2adcbb11-89c4-451e-97f2-14049154ccad`" Version=`"$version`""
 }
 
-
 function CollectNpp($is64)
 {
     $origin = $pwd
@@ -32,8 +31,29 @@ function CollectNpp($is64)
     $outputMod   = if ($is64) { "\x64" }    else { "" }
     $zipSuffix   = if ($is64) { "x64.zip" } else { "x86.zip" }
     $zipFileName = "CodeAlignmentNpp_$($env:APPVEYOR_REPO_TAG_NAME)_$zipSuffix"
-    cd "CodeAlignment.Npp\bin$outputMod\$env:CONFIGURATION"
-    7z a -r $zipFileName *.dll
+
+    $binDir = "CodeAlignment.Npp\bin$outputMod\$env:CONFIGURATION"
+    cd $binDir
+
+    $staging = "staging_release"
+    $targetPluginDir = "$staging\CodeAlignmentNpp"
+    New-Item -ItemType Directory -Path "$targetPluginDir\CodeAlignment" -Force | Out-Null
+
+    if (Test-Path "CodeAlignmentNpp.dll") {
+        Copy-Item "CodeAlignmentNpp.dll" -Destination "$targetPluginDir\" -Force
+    }
+    if (Test-Path "CodeAlignment\CodeAlignment.Common.dll") {
+        Copy-Item "CodeAlignment\CodeAlignment.Common.dll" -Destination "$targetPluginDir\CodeAlignment\" -Force
+    }
+    if (Test-Path "CodeAlignment\CodeAlignment.Common.WinForms.dll") {
+        Copy-Item "CodeAlignment\CodeAlignment.Common.WinForms.dll" -Destination "$targetPluginDir\CodeAlignment\" -Force
+    }
+
+    7z a $zipFileName "./$staging/*"
+
     Push-AppveyorArtifact $zipFileName -FileName $zipFileName
+
+    Remove-Item $staging -Recurse -Force -ErrorAction SilentlyContinue
+
     cd $origin
 }
